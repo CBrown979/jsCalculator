04c473bf108b49a5f8b2968d84e586bd1b6f3120
Candice Bianca Brown <candicebrown97@gmail.com>
2017-12-25 21:55:12 +0000

    gave functionality to 7 button when clicked - displays 7, or multiple 7s if clicked multiple times

--- jsCalc.js
+++ jsCalc.js
@@ -1,21 +1,8 @@
-document.getElementById("submit").onclick = function(){
-    //take the numbers from text field & display them in our display area
-    var input = document.getElementById("input").value;
-    //console.log(input);
-    document.getElementById("display").innerHTML += input;
-    document.getElementById("input").value = "";
+function click7(){
+    var seven = document.getElementById("num7").innerHTML;
+    console.log(seven);
+    document.getElementById("display").innerHTML += seven;
+    console.log("hi");
+    document.getElementById("num7").value="";
 }
 
-document.getElementById("addDigits").onclick = function(){
-    //console.log("addDigits clicked");
-    //get the sum of the digits from the display field
-    var display = document.getElementById("display").innerHTML;
-    //console.log(display);
-    var runningTotal=0;
-    for (var i = 0; i<display.length; i++){
-        //console.log(display[i]);
-        runningTotal += parseInt(display[i]);
-    }
-    console.log(runningTotal);
-    document.getElementById("display").innerHTML=runningTotal;
-}
